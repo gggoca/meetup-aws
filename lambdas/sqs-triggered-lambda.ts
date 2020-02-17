f0b7e91b2204b1c62f9516dcf0a91e5699bc6654
Gordana Gardic <g.gardic@levi9.com>
2020-02-17 17:06:12 +0100

    Renaming folder

--- lambdas/sqs-triggered-lambda.ts
+++ lambdas/sqs-triggered-lambda.ts
@@ -1,3 +1,5 @@
+/* import { SQSEvent, Context, SQSRecord } from 'aws-lambda';
+*/
 export const HelloYou = async () => {
   console.log('Hello you! I am lambda! Nice to meet you. :D ');
   return 'Nothing to see here.'
